f41536bc0b3b0865faf28a308c7ff44464cad266
mdrobinson4 <mdrobinson4@crimson.ua.edu>
2019-08-30 00:10:26 -0500

    threading working

--- client1.c
+++ client1.c
@@ -8,11 +8,23 @@
 #define PORT 8080
 
 struct thread_args {
-	char filename[40];
+	int tile_num;
 };
 
-int sendGOP(struct sockaddr_in servaddr, int client_sock, char *buffer, int file_size, FILE *fp) {
-	int packet_size = 0, bytes = 0;
+int sendGOP(struct sockaddr_in servaddr, int client_sock, char *buffer, int port) {
+	int packet_size = 0, bytes = 0, file_size = 0;
+	FILE *fp = 0;
+
+	/* open file to send */
+	fp = fopen(port, "r");
+	if (fp == NULL)
+		return 0;
+
+	// get the file size
+	fseek(fp, 0, SEEK_END);
+	file_size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
 	/* read in the file and send it to the server */
 	while(fread(buffer, 1, sizeof(buffer), fp) > 0 ) {
 		// calculate the size of the packet to be sent
@@ -26,7 +38,6 @@ int sendGOP(struct sockaddr_in servaddr, int client_sock, char *buffer, int file
 		memset(buffer, 0, sizeof(buffer));
 	}
 	fclose(fp);
-
 }
 
 /* read the file to send and send it to server  */
@@ -34,7 +45,6 @@ void *sendThread(void *arguments) {
 	int buffer_size = 64000, file_size = 0, client_sock = 0;
 	char buffer[buffer_size];
 	struct sockaddr_in servaddr;
-	FILE *fp = NULL;
 
 	struct thread_args *args = arguments;
 
@@ -46,36 +56,24 @@ void *sendThread(void *arguments) {
 
 	/* configure socket */
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(PORT);
+	servaddr.sin_port = htons(PORT + args->tile_num);
 	servaddr.sin_addr.s_addr = INADDR_ANY;
 
-	/* open file to send */
-	fp = fopen(args->filename, "r");
-	if (fp == NULL)
-		return 0;
-
-	// get the file size
-	fseek(fp, 0, SEEK_END);
-	file_size = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-
 	// int sendGOP(struct sockaddr_in servaddr, int client_sock, char *buffer, int file_size, FILE *fp)
-	sendGOP(servaddr, client_sock, buffer, file_size, fp);
-
+	sendGOP(servaddr, client_sock, buffer, PORT + args->tile_num);
 	return 0;
 }
 
 int main(int argc, char const *argv[]) {
-		char *filename;
-		int tile_count = 0, i = 0;
+		int tile_count = 60, i = 0;
 		pthread_t thread_array[tile_count];
-		struct thread_args args;
+		struct thread_args args[tile_count];
 
 		///////////////   LOOP   ///////////////////////////////////
 		for (i = 0; i < tile_count; i++) {
-			sprintf(args.filename, "%d", i);
+			args[i].tile_num = i;
 			/* create thread to send videos */
-			pthread_create(&thread_array[i], NULL, &sendThread, (void *)&args);
+			pthread_create(&thread_array[i], NULL, &sendThread, (void *)&args[i]);
 		}
 		/* wait for threads to end */
 		for ( i = 0; i < tile_count; i++) {
--- server1.c
+++ server1.c
@@ -5,29 +5,29 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <pthread.h>
-#define PORT 8000
+#define PORT 8080
 
 struct thread_args {
 	int tile_num;
-	char filename[40];
 };
 
-int getGOP(int server_sock, char *buffer, int buffer_size, FILE *fp) {
+int getGOP(int server_sock, char *buffer, int buffer_size, int port) {
 	int len = 0, bytes = 0;
+	FILE *fp = 0;
+	fp = fopen(port, "w");
 	struct sockaddr_in cliaddr;
+
 	// wait to get first packet of data (block)
-	printf("Listening!\n");
 	bytes = recvfrom(server_sock, buffer, buffer_size, 0, (struct sockaddr*)&cliaddr, &len);
 	/*	set timeout	after initial read  */
 	struct timeval timeout;
 	timeout.tv_sec = 0;
 	timeout.tv_usec = 100000;
-/*
 	if (setsockopt(server_sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0) {
 		perror("Error");
 		return -1;
 	}
-*/
+	// get additional packets
 	do {
 		// write the received packet to a file
 		fwrite(buffer, 1, bytes, fp);
@@ -35,7 +35,6 @@ int getGOP(int server_sock, char *buffer, int buffer_size, FILE *fp) {
 		memset(buffer, 0, sizeof(buffer));
 		// get additional packets (don't block)
 	} while ((bytes = recvfrom(server_sock, buffer, buffer_size, 0, (struct sockaddr*)&cliaddr, &len)) > 0);
-
 	fclose(fp);
 	return 0;
 }
@@ -45,56 +44,38 @@ void *receiveThread(void *arguments) {
 		struct sockaddr_in servaddr;
 		int buffer_size = 64000;
 		char buffer[buffer_size];
-		FILE *fp = NULL;
 
 		struct thread_args *args = arguments;
-		fp = fopen(args->filename, "w");
-
 		// Creating socket file descriptor
 		if ((server_sock = socket(AF_INET, SOCK_DGRAM, 0)) == 0) {
 				perror("socket failed");
 				exit(EXIT_FAILURE);
 		}
-
 		// configure server socket
 		servaddr.sin_family = AF_INET;
 		servaddr.sin_addr.s_addr = INADDR_ANY;
-		servaddr.sin_port = htons(PORT);
-
-		int optval = 1;
-		setsockopt(server_sock, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
-
-		// SO_REUSEPORT
-
-		// bind the socket to port 8080
+		servaddr.sin_port = htons(PORT + args->tile_num);
+		// bind the socket to the specified port
 		if (bind(server_sock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
 				perror("bind failed");
 				exit(EXIT_FAILURE);
 		}
-		else {
-			printf("Server Created\n");
-		}
-
-		// int getGOP(int server_sock, char *buffer, int buffer_size, FILE *fp)
-		getGOP(args->server_sock, buffer, buffer_size, fp);
+		// listen for messages at specified port
+		getGOP(server_sock, buffer, buffer_size, PORT + args->tile_num);
 }
 
 // fuser -TERM 8080/udp
 
 int main(int argc, char const *argv[]) {
-		char *filename;
 		int tile_count = 60, i = 0;
-		pthread_t thread_array[tile_count];
-		struct thread_args args;
-		struct sockaddr_in servaddr[tile_count];
+		pthread_t thread_array[tile_count];		// array for each thread
+		struct thread_args args[tile_count];	// array for argument structs
 
-		///////////////   LOOP   ///////////////////////////////////
+		/* create udp threads all listening for input */
 		for (i = 0; i < tile_count; i++) {
-			sprintf(args[i].filename, "%d", i);
-			args.tile_num = i;
+			args[i].tile_num = i;
 			/* create thread to send videos */
-			pthread_create(&thread_array[i], NULL, &receiveThread, (void *)&args);
-			/* wait for thread to return */
+			pthread_create(&thread_array[i], NULL, &receiveThread, (void *)&args[i]);
 		}
 		/* wait for threads to end */
 		for ( i = 0; i < tile_count; i++) {
